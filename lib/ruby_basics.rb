8022a4daf784f696ef29e0a404b5ca08206765b2
Buntry <parkergriep@yahoo.com>
2016-05-28 14:52:06 -0400

    lab complete

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+    num1/num2
 end
 
 def assign_variable(value)
-  
+    v = value
+    v
 end
 
-def argue
-  
+def argue(x)
+    x
 end
 
-def greeting
-
+def greeting(g, n)
+    g + n
 end
 
 def return_a_value
-
+    "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value()
+    "expert"
 end
 
-def pizza_party
-
+def pizza_party(a = "cheese")
+    a
 end
